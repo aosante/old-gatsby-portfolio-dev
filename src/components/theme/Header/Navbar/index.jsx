3920a980d6d19913b97510f7ee11d20bb704502c
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-08-01 21:27:05 -0600

    initial commit

---
+++ src/components/theme/Header/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from 'components/common'
+import logo from 'assets/illustrations/logo.png'
+import NavbarLinks from '../NavbarLinks'
+import { Wrapper } from './styles'
+
+const Navbar = () => (
+	<Wrapper as={Container}>
+		<img src={logo} alt="Website logo" />
+		<NavbarLinks desktop />
+	</Wrapper>
+)
+
+export default Navbar
---
+++ src/data/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  defaultTitle: 'Andrés',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'Andrés',
+  url: 'https://portfolio.smakosh.com',
+  legalName: 'John Doe',
+  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/smakosh',
+    github: 'https://github.com/aosante',
+    linkedin: 'https://www.linkedin.com/in/andr%C3%A9s-osante-alfaro-845701ba/',
+    instagram: 'https://www.instagram.com/aosanaa/',
+    youtube: '',
+    google: '',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#da328a',
+  backgroundColor: '#da328a',
+  social: {
+    facebook: 'appId',
+    twitter: '',
+  },
+  address: {
+    city: 'San José',
+    region: 'Santa Ana',
+    country: 'Costa Rica',
+    zipCode: '10901',
+  },
+  contact: {
+    email: 'aosantea@ucenfotec.ac.cr',
+    phone: '+506 8814-5840',
+  },
+  foundingDate: '2020',
+  recaptcha_key: '6LfuIbkZAAAAAOjlJsaTeDsW2EFB3UfyOJcFbTpm',
+};
