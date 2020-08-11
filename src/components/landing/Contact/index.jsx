8880d42fdc0ecd7dc10ac6f4e451f475f98de148
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-08-10 22:32:23 -0600

    replaced contact section svg

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container } from 'components/common'
-import contact from 'assets/illustrations/contact.svg'
+import contact from 'assets/illustrations/contact2.svg'
 import { Wrapper, Details, Thumbnail } from './styles'
 import ContactForm from './ContactForm'
 
