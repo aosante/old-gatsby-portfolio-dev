3afa627ff9e9c70cc1ec98960e980bc92b5eda85
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-09-03 22:10:28 -0600

    added animation on about section

--- src/components/landing/About/index.jsx
+++ src/components/landing/About/index.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import {InView} from 'react-intersection-observer'
+import {useSpring, animated} from 'react-spring'
 
 import { Container, Button } from 'components/common'
 import surfer from 'assets/illustrations/surfer.svg'
 import {  AboutWrapper, Details, Thumbnail } from './styles'
 
-export const About = () => (
+export const About = () =>  {
+  const [showP, setShowP] = useState(false);
+  const aboutProps = useSpring({opacity: showP ? 1 : 0, transform: showP ? 'translateX(0)' : 'translateX(200px)', from: {opacity: 0}, config: {duration: 700},delay: 700});
+
+  const onViewChange = inview => {
+    if(!showP && inview) setShowP(true);
+  }
+  
+  return (
   <div id="about">
     <AboutWrapper as={Container}>
       <Thumbnail>
         <img src={surfer} alt="Surfer illustration" />
       </Thumbnail>
       <Details>
-        <h1>About Me</h1>
-        <p>
-        I'm a business graduate from <a href="https://www.ucr.ac.cr/" target="_blank">Universidad de Costa Rica</a>, with a technical degree in Sofwtare Engineering from <a href="https://www.ucenfotec.ac.cr/" target="_blank">Universidad Cenfotec</a>.
-        </p>
-        <p>I discovered software development 4 years ago, and I've been building apps as a hobby and professionally ever since.</p>
-        <p>Surfing, guitar and programming are the three things I enjoy the most, and I look forward to excercising my passion for the latter by working with you!</p>
+        <InView tag="div">
+          <h1>About Me</h1>
+        </InView>
+        <InView tag="div" onChange={onViewChange}>
+          <animated.div style={aboutProps}>
+            <p>
+            I'm a business graduate from <a href="https://www.ucr.ac.cr/" target="_blank">Universidad de Costa Rica</a>, with a technical degree in Sofwtare Engineering from <a href="https://www.ucenfotec.ac.cr/" target="_blank">Universidad Cenfotec</a>.
+            </p>
+            <p>I discovered software development 4 years ago, and I've been building apps as a hobby and professionally ever since.</p>
+            <p>Surfing, guitar and programming are the three things I enjoy the most, and I look forward to excercising my passion for the latter by working with you!</p>
+          </animated.div>
+        </InView>
         <Button as={AnchorLink} href="#contact">
           Let's Talk
         </Button>
@@ -25,3 +41,4 @@ export const About = () => (
     </AboutWrapper>
   </div>
 )
+  };
