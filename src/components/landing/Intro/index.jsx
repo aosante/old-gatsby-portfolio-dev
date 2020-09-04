247871c4129f6091461359a164e783d8d77a5a89
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-09-03 18:04:34 -0600

    added react spring animations to hero section

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import {useSpring, animated} from 'react-spring'
 
 import { Header } from 'components/theme'
 import { Container, Button } from 'components/common'
 import dev from 'assets/illustrations/developer.svg'
 import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles'
 
-export const Intro = () => (
+const titleAnimation = (animationDuration, animationDelay) => {
+	return {
+		opacity: 1,
+		transform: 'translateX(0)',
+		from: {opacity: 0,
+		transform: 'translateX(-200px)',},
+		config: {duration: animationDuration},
+		delay: animationDelay && animationDelay
+	}
+}
+
+export const Intro = () => {
+	const props = useSpring(titleAnimation(800));
+	const headingProps = useSpring(titleAnimation(1000, 700));
+	const buttonProps = useSpring({opacity:  1, from: {opacity: 0}, config: {duration: 1050}, delay: 1500})
+
+	// redefine button to be able to animate
+	const AnimatedButton = animated(Button);
+
+ return (
 	<Wrapper>
 		<Header />
 		<IntroWrapper as={Container}>
 			<Details>
 				<p>Hi there! I'm</p>
-				<h1>Andrés.</h1>
-				<h4>A developer from <span>Costa Rica.</span></h4>
+				<animated.h1 style={props}>Andrés.</animated.h1>
+				<animated.h4  style={headingProps}>A developer from <span>Costa Rica.</span></animated.h4>
 				<div>
-				<Button as={AnchorLink} href="#about">
+				<AnimatedButton style={buttonProps} as={AnchorLink} href="#about">
 					Learn More
-				</Button>
+				</AnimatedButton>
 				</div>
 			</Details>
 			<Thumbnail>
@@ -26,3 +46,4 @@ export const Intro = () => (
 		</IntroWrapper>
 	</Wrapper>
 )
+ }
