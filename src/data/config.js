1a166cb3b9f5a8e94ac802ebe55aeed57a11f907
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-09-04 23:34:43 -0600

    replaced diferrent sized svg icons

--- src/data/config.js
+++ src/data/config.js
@@ -39,14 +39,12 @@ module.exports = {
     css: 'linear-gradient(135deg, #a8edea 0%, #fed6e3 100%)',
     height: 250,
     name: 'htmlIcon',
-    isDark: true
   },
   {
     
     css: 'linear-gradient(135deg, #c3cfe2 0%, #c3cfe2 100%)',
     height: 250,
     name: 'cssIcon',
-    isDark: true
   },
   {
     
