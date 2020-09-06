24b7a82ae0645ec209536002cab8bea9ce08552a
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-09-05 22:49:49 -0600

    addded react-hook-form form

--- src/components/common/Input/index.js
+++ src/components/common/Input/index.js
@@ -19,3 +19,24 @@ export const Input = styled.input`
     color: #a7a7a7;
   }
 `;
+
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  box-sizing: border-box;
+  border: 2px solid #6c63ff;
+  padding: 0.8rem 1rem;
+  border-radius: 7px;
+  margin-bottom: 0.5rem;
+  transition: 0.3s;
+
+  ${({ error }) =>
+    error &&
+    `
+		border-color: #ff4136;
+	`}
+
+  &::placeholder {
+    color: #a7a7a7;
+  }
+`;
--- src/components/landing/Contact/ContactForm/index.jsx
+++ src/components/landing/Contact/ContactForm/index.jsx
@@ -1,141 +1,78 @@
-import React from 'react'
-import { Form, withFormik, FastField, ErrorMessage } from 'formik'
-import Recaptcha from 'react-google-recaptcha'
-import * as Yup from 'yup'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-import { Button, Input } from 'components/common'
-import { recaptcha_key } from 'data/config'
-import { Error, Center, InputField } from './styles'
+import { Button, Input, TextArea } from 'components/common';
+import { Error, Center, InputField } from './styles';
 
-const ContactForm = ({
-	setFieldValue,
-	isSubmitting,
-	values,
-	errors,
-	touched,
-}) => (
-	<Form
-		name="portfolio-dev"
-		method="post"
-		data-netlify="true"
-		data-netlify-recaptcha="true"
-		data-netlify-honeypot="bot-field"
-	>
-		<InputField>
-			<Input
-				as={FastField}
-				type="text"
-				name="name"
-				component="input"
-				aria-label="name"
-				placeholder="Full name*"
-				error={touched.name && errors.name}
-			/>
-			<ErrorMessage component={Error} name="name" />
-		</InputField>
-		<InputField>
-			<Input
-				id="email"
-				aria-label="email"
-				component="input"
-				as={FastField}
-				type="email"
-				name="email"
-				placeholder="Email*"
-				error={touched.email && errors.email}
-			/>
-			<ErrorMessage component={Error} name="email" />
-		</InputField>
-		<InputField>
-			<Input
-				as={FastField}
-				component="textarea"
-				aria-label="message"
-				id="message"
-				rows="8"
-				type="text"
-				name="message"
-				placeholder="Message*"
-				error={touched.message && errors.message}
-			/>
-			<ErrorMessage component={Error} name="message" />
-		</InputField>
-		{values.name && values.email && values.message && (
-			<InputField>
-				<FastField
-					component={Recaptcha}
-					sitekey={recaptcha_key}
-					name="recaptcha"
-					onChange={value => setFieldValue('recaptcha', value)}
-				/>
-				<ErrorMessage component={Error} name="recaptcha" />
-			</InputField>
-		)}
-		{values.success && (
-			<InputField>
-				<Center>
-					<h4>
-						Your message has been successfully sent, I will get back to you
-						ASAP!
-					</h4>
-				</Center>
-			</InputField>
-		)}
-		<Center>
-			<Button secondary type="submit" disabled={isSubmitting}>
-				Submit
-			</Button>
-		</Center>
-	</Form>
-)
+const ContactForm = () => {
+  const { handleSubmit, errors, register } = useForm({ mode: 'onTouched' });
 
-export default withFormik({
-	mapPropsToValues: () => ({
-		name: '',
-		email: '',
-		message: '',
-		recaptcha: '',
-		success: false,
-	}),
-	validationSchema: () =>
-		Yup.object().shape({
-			name: Yup.string().required('Full name field is required'),
-			email: Yup.string()
-				.email('Invalid email')
-				.required('Email field is required'),
-			message: Yup.string().required('Message field is required'),
-			recaptcha: Yup.string().required('Robots are not welcome yet!'),
-		}),
-	handleSubmit: async (
-		{ name, email, message, recaptcha },
-		{ setSubmitting, resetForm, setFieldValue }
-	) => {
-		try {
-			const encode = data => {
-				return Object.keys(data)
-					.map(
-						key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
-					)
-					.join('&')
-			}
-			await fetch('/?no-cache=1', {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-				body: encode({
-					'form-name': 'portfolio-dev',
-					name,
-					email,
-					message,
-					'g-recaptcha-response': recaptcha,
-				}),
-			})
-			await setSubmitting(false)
-			await setFieldValue('success', true)
-			setTimeout(() => resetForm(), 2000)
-		} catch (err) {
-			setSubmitting(false)
-			setFieldValue('success', false)
-			alert('Something went wrong, please try again!') // eslint-disable-line
-		}
-	},
-})(ContactForm)
+  const onSubmit = data => {
+    console.log(data);
+    // fetch("/", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //   body: encode({ "form-name": "contact", data })
+    // })
+    //   .then(() => alert("Success!"))
+    //   .catch(error => alert(error));
+  }
+  return (
+    <form
+      name="portfolio-dev"
+      method="post"
+      data-netlify="true"
+      data-netlify-recaptcha="true"
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <InputField>
+        <Input
+          type="text"
+          name="name"
+          aria-label="name"
+          placeholder="Full name*"
+          ref={register({ required: true })}
+          error={errors.name ? true : false}
+        />
+        <Error>{errors.name && 'Your name is required'}</Error>
+      </InputField>
+      <InputField>
+        <Input
+          id="email"
+          aria-label="email"
+          type="email"
+          name="email"
+          placeholder="Email*"
+          ref={register({ required: true, pattern: /^\S+@\S+\.\S+$/})}
+          error={errors.email ? true : false}
+        />
+        <Error>
+          {errors.email && 'You must provide a valid email address'}
+        </Error>
+      </InputField>
+      <InputField>
+        <TextArea
+          aria-label="message"
+          id="message"
+          rows="8"
+          type="message"
+          name="message"
+          placeholder="Message*"
+          ref={register({ required: true })}
+          error={errors.message ? true : false}
+        />
+        <Error>{errors.message && 'This field is required'}</Error>
+      </InputField>
+      {/* TODO: Add recaptcha here */}
+      <Center>
+        <Button secondary type="submit">
+          Submit
+        </Button>
+      </Center>
+    </form>
+  );
+};
+
+export default ContactForm;
