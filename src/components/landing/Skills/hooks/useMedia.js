015b5b1f2f931eab05306787b40f00c34cb561df
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-09-05 19:22:44 -0600

    fixed useMedia hook bug

--- src/components/landing/Skills/hooks/useMedia.js
+++ src/components/landing/Skills/hooks/useMedia.js
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react'
 
 export default function useMedia(queries, values, defaultValue) {
-  const match = () => values[queries.findIndex(q => matchMedia(q).matches)] || defaultValue
+  const match = () => values[queries.findIndex(q => {
+    if(typeof window !== 'undefined') {
+      return matchMedia(q).matches;
+    } else {
+      return -1;
+    }
+  })] || defaultValue
   const [value, set] = useState(match);
   useEffect(() => {
     const handler = () => set(match);
